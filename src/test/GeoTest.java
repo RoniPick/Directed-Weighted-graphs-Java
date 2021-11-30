d61308a59a0a4c9a37bd0a4bf3d05a9e637d6609
RoniPick <92802335+RoniPick@users.noreply.github.com>
2021-11-30 19:31:50 +0200

    test added

---
+++ src/test/GeoTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GeoTest {
+
+    @Test
+    void x() {
+    }
+
+    @Test
+    void y() {
+    }
+
+    @Test
+    void z() {
+    }
+
+    @Test
+    void setX() {
+    }
+
+    @Test
+    void setY() {
+    }
+
+    @Test
+    void setZ() {
+    }
+
+    @Test
+    void distance() {
+    }
+}
---
+++ src/test/GraphTest.java
@@ -0,0 +1,65 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GraphTest {
+
+    @Test
+    void getNode() {
+    }
+
+    @Test
+    void getEdge() {
+    }
+
+    @Test
+    void addNode() {
+    }
+
+    @Test
+    void connect() {
+    }
+
+    @Test
+    void nodeIter() {
+    }
+
+    @Test
+    void edgeIter() {
+    }
+
+    @Test
+    void testEdgeIter() {
+    }
+
+    @Test
+    void removeNode() {
+    }
+
+    @Test
+    void removeEdge() {
+    }
+
+    @Test
+    void nodeSize() {
+    }
+
+    @Test
+    void edgeSize() {
+    }
+
+    @Test
+    void getMC() {
+        Graph g = new Graph();
+        int a=0;
+        assertEquals(a, g.getMC());
+        Geo n = new Geo();
+        Node node = new Node(1, 1.233, 1, n);
+        g.addNode(node);
+        a++;
+        assertEquals(a, g.getMC());
+        g.removeNode(node.getKey());
+        a++;
+        assertEquals(a, g.getMC());
+    }
+}
