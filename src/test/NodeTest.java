61b612bd7f45c43cd6d6cf91fe36bf26f91167ec
Almog-David <almog.david99@gmail.com>
2021-12-02 16:39:12 +0200

    uml + Nodetest

--- src/test/NodeTest.java
+++ src/test/NodeTest.java
@@ -33,39 +33,63 @@ class NodeTest {
         n = new Node(1,20,1,g);
         Geo x = new Geo(2.0,3.0,1.0);
         n.setLocation(x);
-        assertEquals(x,n.getLocation());
+        assertEquals(x.x,n.getLocation().x());
+        assertEquals(x.y,n.getLocation().y());
+        assertEquals(x.z,n.getLocation().z());
 
         Geo y = new Geo (3.0,1.5,4.7);
         n.setLocation(y);
-        assertNotEquals(x,n.getLocation());
+        assertNotEquals(x.x,n.getLocation().x());
+        assertNotEquals(x.y,n.getLocation().y());
+        assertNotEquals(x.z,n.getLocation().z());
     }
 
     @Test
     void getWeight() {
         assertEquals(0,n.getWeight());
 
-        n = new Node(1,25,0,g);
-        assertNotEquals(0,n.getWeight());
-        assertEquals(25,n.getWeight());
+        Node d = new Node(1,25,0,g);
+        assertNotEquals(0,d.getWeight());
+        assertEquals(25,d.getWeight());
     }
 
     @Test
     void setWeight() {
+        n.setWeight(55);
+        assertEquals(55,n.getWeight());
+        assertNotEquals(0,n.getWeight());
+
+        n.setWeight(21);
+        n.setWeight(22);
+        assertEquals(22,n.getWeight());
+        assertNotEquals(21,n.getWeight());
+        assertNotEquals(55,n.getWeight());
     }
 
     @Test
-    void getInfo() {
-    }
+    void getInfo() {} // not relevant
 
     @Test
-    void setInfo() {
-    }
+    void setInfo() {} // not relevant
 
     @Test
     void getTag() {
+        assertEquals(0,n.getTag());
+
+        Node d = new Node(1,25,0,g);
+        assertEquals(0, d.getTag());
+        assertNotEquals(1, d.getTag());
     }
 
     @Test
     void setTag() {
+        n.setTag(3);
+        assertNotEquals(1, n.getTag());
+
+        n.setTag(2);
+        n.setTag(5);
+        assertNotEquals(3, n.getTag());
+        assertNotEquals(2, n.getTag());
+        assertEquals(5, n.getTag());
     }
 }
