e482b6ebd006d8ed5fe9725080fc3ae5392fb701
RoniPick <92802335+RoniPick@users.noreply.github.com>
2021-12-09 13:07:17 +0200

    continue GUI implement

--- src/GUI.java
+++ src/GUI.java
@@ -1,17 +1,24 @@
+import api.EdgeData;
 import api.NodeData;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Line2D;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class GUI {
+public class GUI extends JFrame implements ActionListener{
 
+    //in order to run the GUI program
+    public static void runGUI(Graph g){
+        new GUI(g);
+    }
 
-    public class MyFrame extends JFrame implements ActionListener {
+//    public class MyFrame  {
         JMenuBar menu ; //for the menu bar
         JMenu file;
         JMenu edit;
@@ -51,15 +58,23 @@ public class GUI {
 
 
 
-        MyFrame(Graph graph){
+         public GUI(Graph graph){
+             JPanel panel = new JPanel(new BorderLayout());
+             jp = new JPanel();
+             jp.setBackground(new Color(255, 255, 255));
+             Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize(); // got from Amichai's tirgul
+             jp.setBounds(screenSize.width/2-100, 0, 100, screenSize.width/2);// got from Amichai's tirgul
             this.setTitle("Ex2"); //sets title of frame
             this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit from application
             this.setLayout(null);
-            this.setSize(600,600); //sets the x-dimension, and y-dimension of frame
+            this.setSize(screenSize.width/2,screenSize.height/2); //sets the x-dimension, and y-dimension of frame - took from Amichai's tirgul
             this.getContentPane().setBackground(new Color(255, 255, 255)); //change color of background to white
-            this.setResizable(false); //frame can not be resized
-            jp = new JPanel();
-            this.setVisible(true); //make frame visible
+            this.setResizable(true); //frame can be resized
+             this.add(jp);
+             this.add(panel);
+             this.gaMain = new GraphAlgorithms(graph);
+             this.gaCopy = gaMain;
+
 
             menu = new JMenuBar();
             file = new JMenu("File");
@@ -94,7 +109,8 @@ public class GUI {
             menu.add(file); menu.add(edit); menu.add(algorithms); menu.add(help);
             this.setJMenuBar(menu);
 
-            this.setVisible(false); //make frame visible
+             this.setResizable(false); //frame can not be resized
+             this.setVisible(true); //make frame visible
 
         }
 
@@ -113,9 +129,14 @@ public class GUI {
                     String getPath = jf.getSelectedFile().getAbsolutePath();
                     this.gaMain.load(getPath);
 
-//                     try{
-//
-//                     }
+                     try{
+                         runGUI((Graph)this.gaMain.getGraph());
+                         setVisible(false);
+                         dispose();
+                     }
+                     catch (Exception exception){
+                         exception.printStackTrace();
+                     }
             }
             }
 
@@ -135,23 +156,19 @@ public class GUI {
                 label1.setBounds(10, 10, 100, 20);
                 this.add(label1);
 
-
                 firstNode = new JTextField();
                 this.firstNode.setBounds(130, 10, 50, 20);
                 this.add(firstNode);
                 String nodeSrc = firstNode.getText();
-                int src = Integer.parseInt(nodeSrc);
 
                 label2 = new JLabel("Enter Second Node:");
                 label2.setBounds(10, 50, 130, 20);
                 this.add(label2);
 
-
                 secondNode = new JTextField();
                 this.secondNode.setBounds(130, 50, 50, 20);
                 this.add(secondNode);
                 String nodeDest = secondNode.getText();
-                int dest = Integer.parseInt(nodeDest);
 
                 button = new JButton();
                 button.setText("Remove edge");
@@ -166,6 +183,8 @@ public class GUI {
                     repaint();
                 }
                 else{
+                    int src = Integer.parseInt(nodeSrc);
+                    int dest = Integer.parseInt(nodeDest);
                     this.gaMain.getGraph().removeEdge(src, dest);
                     repaint();
                 }
@@ -185,7 +204,6 @@ public class GUI {
                 this.xValue.setBounds(60, 10, 50, 20);
                 this.add(xValue);
                 String ID = xValue.getText();
-                int id = Integer.parseInt(ID);
 
     //                label2 = new JLabel("Enter Y:");
     //                label2.setBounds(10, 50, 50, 20);
@@ -210,6 +228,7 @@ public class GUI {
                     repaint();
                 }
                 else{
+                    int id = Integer.parseInt(ID);
                     this.gaMain.getGraph().removeNode(id);
                     repaint();
                 }
@@ -223,23 +242,19 @@ public class GUI {
                 label1.setBounds(10, 10, 100, 20);
                 this.add(label1);
 
-
                 firstNode = new JTextField();
                 this.firstNode.setBounds(130, 10, 50, 20);
                 this.add(firstNode);
                 String nodeSrc = firstNode.getText();
-                int src = Integer.parseInt(nodeSrc);
 
                 label2 = new JLabel("Enter Second Node:");
                 label2.setBounds(10, 50, 130, 20);
                 this.add(label2);
 
-
                 secondNode = new JTextField();
                 this.secondNode.setBounds(130, 50, 50, 20);
                 this.add(secondNode);
                 String nodeDest = secondNode.getText();
-                int dest = Integer.parseInt(nodeDest);
 
                 label3 = new JLabel("Enter Edge Weight:");
                 label3.setBounds(10, 90, 130, 20);
@@ -249,7 +264,6 @@ public class GUI {
                 this.edgeWeight.setBounds(130, 90, 130, 20);
                 this.add(edgeWeight);
                 String weight = edgeWeight.getText();
-                double w = Double.parseDouble(weight);
 
                 button = new JButton();
                 button.setText("Add edge");
@@ -263,6 +277,9 @@ public class GUI {
                     repaint();
                 }
                 else{
+                    int src = Integer.parseInt(nodeSrc);
+                    int dest = Integer.parseInt(nodeDest);
+                    double w = Double.parseDouble(weight);
                     this.gaMain.getGraph().connect(src, dest, w);
                     repaint();
                 }
@@ -281,7 +298,6 @@ public class GUI {
                 this.xValue.setBounds(60, 10, 50, 20);
                 this.add(xValue);
                 String ID = xValue.getText();
-                int id = Integer.parseInt(ID);
 
                 label2 = new JLabel("Enter X:");
                 label2.setBounds(10, 50, 50, 20);
@@ -292,7 +308,6 @@ public class GUI {
                 this.xValue.setBounds(60, 50, 50, 20);
                 this.add(xValue);
                 String nodeX = xValue.getText();
-                int x = Integer.parseInt(nodeX);
 
                 label3 = new JLabel("Enter Y:");
                 label3.setBounds(10, 90, 50, 20);
@@ -303,8 +318,6 @@ public class GUI {
                 this.yValue.setBounds(60, 90, 50, 20);
                 this.add(yValue);
                 String nodeY = yValue.getText();
-                int y = Integer.parseInt(nodeY);
-
 
                 button = new JButton();
                 button.setText("Add Node");
@@ -319,6 +332,9 @@ public class GUI {
                     repaint();
                 }
                 else{
+                    int id = Integer.parseInt(ID);
+                    int x = Integer.parseInt(nodeX);
+                    int y = Integer.parseInt(nodeY);
                     Geo g = new Geo(x, y, 0);
                     NodeData node = new Node(id, 0, 0, g);
                     this.gaMain.getGraph().addNode(node);
@@ -334,29 +350,33 @@ public class GUI {
                 label1.setBounds(10, 10, 100, 20);
                 this.add(label1);
 
-
                 firstNode = new JTextField();
                 this.firstNode.setBounds(130, 10, 50, 20);
                 this.add(firstNode);
                 String nodeSrc = firstNode.getText();
-                int src = Integer.parseInt(nodeSrc);
 
                 label2 = new JLabel("Enter Second Node:");
                 label2.setBounds(10, 50, 130, 20);
                 this.add(label2);
 
-
                 secondNode = new JTextField();
                 this.secondNode.setBounds(130, 50, 50, 20);
                 this.add(secondNode);
                 String nodeDest = secondNode.getText();
-                int dest = Integer.parseInt(nodeDest);
+
+                button = new JButton();
+                button.setText("Find Shortest Path");
+                this.button.setBounds(10, 100, 130, 20);
+                button.addActionListener(this);
+                this.add(button);
 
                 if(nodeSrc.length() == 0 || nodeDest.length() == 0){
                     shortestPath = null;
                     repaint();
                 }
                 else{
+                    int src = Integer.parseInt(nodeSrc);
+                    int dest = Integer.parseInt(nodeDest);
                     shortestPath = (ArrayList<NodeData>) gaMain.shortestPath(src, dest);
                     repaint();
                 }
@@ -376,12 +396,12 @@ public class GUI {
             else if(e.getSource() == connected){
             if(gaMain.isConnected()){
                 label1 = new JLabel("The graph is connected!");
-                label1.setBounds(10, 10, 100, 20);
+                label1.setBounds(10, 10, 200, 20);
                 this.add(label1);
             }
             else {
                 label1 = new JLabel("The graph is NOT connected!");
-                label1.setBounds(10, 10, 100, 20);
+                label1.setBounds(10, 10, 200, 20);
                 this.add(label1);
             }
 
@@ -390,9 +410,99 @@ public class GUI {
 
             }
 
+
+
         }
 
+        //we took ideas from Amichai's tirgul
+        protected void paintComponent(Graphics g){
+             super.paintComponents(g);
+             double x = Math.abs(minX-maxX);
+             double y = Math.abs((minY-maxY));
+             double Xscale = getWidth()/x;
+             double Yscale = getHeight()/y;
+             Node prev = null;
+             Graphics2D graphics2D = (Graphics2D) g;
+             graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
+             Iterator<EdgeData> iter = gaMain.getGraph().edgeIter();
+             while(iter.hasNext()){
+                 EdgeData edge = iter.next();
+
+                 //adjusting the graph to the screen scale
+                 double xSrc = (gaMain.getGraph().getNode(edge.getSrc()).getLocation().x() - minX) * Xscale;
+                 double xDest = (gaMain.getGraph().getNode(edge.getDest()).getLocation().x() - minX) * Xscale;
+                 double ySrc = (gaMain.getGraph().getNode(edge.getSrc()).getLocation().y() - minY) * Yscale;
+                 double yDest = (gaMain.getGraph().getNode(edge.getDest()).getLocation().y() - minY) * Yscale;
+
+                 g.setColor(new Color(8, 6, 17));
+                 graphics2D.setStroke(new BasicStroke(1));
+                 graphics2D.draw(new Line2D.Double((int)xSrc, (int)ySrc, (int)xDest, (int)yDest));
+
+                 drawArrowLine((Graphics2D) g,(int)xSrc, (int)ySrc, (int)xDest, (int)yDest);
+                 graphics2D.dispose();
+
+             }
+
+
+             if(shortestPath != null){
+                 for(int i=0; i<shortestPath.size()-1; i++){
+                     EdgeData temp = gaMain.getGraph().getEdge(shortestPath.get(i).getKey(), shortestPath.get(i+1).getKey());
+
+                     //adjusting the graph to the screen scale
+                     double xSrc = (gaMain.getGraph().getNode(temp.getSrc()).getLocation().x() - minX) * Xscale;
+                     double xDest = (gaMain.getGraph().getNode(temp.getDest()).getLocation().x() - minX) * Xscale;
+                     double ySrc = (gaMain.getGraph().getNode(temp.getSrc()).getLocation().y() - minY) * Yscale;
+                     double yDest = (gaMain.getGraph().getNode(temp.getDest()).getLocation().y() - minY) * Yscale;
+
+                     g.setColor(new Color(25, 4, 92));
+                     graphics2D.draw(new Line2D.Double((int)xSrc, (int)ySrc, (int)xDest, (int)yDest));
+                 }
+
+             }
+
+            Iterator<NodeData> iterator = gaMain.getGraph().nodeIter();
+            while(iterator.hasNext()){
+                NodeData node = iterator.next();
+                double X = (node.getLocation().x() - minX) * Xscale; //adjusting the graph to the screen scale
+                double Y = (node.getLocation().y() - minY) * Yscale;//adjusting the graph to the screen scale
+                g.setColor(new Color(204, 136, 231));
+                g.fillOval((int)x, (int)y, 20, 20);
+                g.setColor(new Color(63, 28, 76));
+                g.drawString(node.getKey() + "", (int) X, (int)Y);
+
+            }
+
+            if(this.graphCenter != null){
+                double X = (graphCenter.getLocation().x() - minX) * Xscale;
+                double Y = (graphCenter.getLocation().y() - minY) * Yscale;
+                g.setColor(new Color(203, 25, 61));
+                g.fillOval((int)X, (int)Y, 20, 20);
+                g.drawString("Center Node", (int)X-15, (int)Y);
+            }
+
+
+
+        }
+
+        // took from https://itqna.net/questions/3389/how-draw-arrow-using-java2d
+        public void drawArrowLine(Graphics2D g,int xSrc, int ySrc, int xDest, int yDest){
+            Polygon arrowHead = new Polygon();
+            AffineTransform tx = new AffineTransform();
+            arrowHead.addPoint(0, 5);
+            arrowHead.addPoint(-5, -5);
+            arrowHead.addPoint(5, -5);
+
+            tx.setToIdentity();
+            double angle = Math.atan2(yDest - ySrc, xDest - xSrc);
+            tx.translate(xDest, yDest);
+            tx.rotate(angle - Math.PI / 2d);
+
+            g.setTransform(tx);
+            g.fill(arrowHead);
+
+
+        }
 
         //setting the scale of the graph
         public void GraphScale(Graph graph){
@@ -452,16 +562,26 @@ public class GUI {
 
         }
 
-    }
 
 
-//    public class GraphScale extends JPanel{
-//
 //    }
 
 
+
+
+
     public static void main(String[] args) {
-        //MyFrame frame = new MyFrame(); //creates a frame
+             Graph g = new Graph();
+        g.addNode(new Node(0, 1.2, 0, new Geo(675,998,0.0)));
+        g.addNode(new Node(1, 1.6, 0, new Geo(876,47,0.0)));
+        g.addNode(new Node(2, 9.2, 0, new Geo(7676,32.10152879327731,0.0)));
+        g.addNode(new Node(3, 1.7, 0, new Geo(88,68,0.0)));
+
+        g.connect(1, 3, 1.9);
+        g.connect(2, 0, 8.9);
+        g.connect(2, 3, 1.66);
+
+        GUI frame = new GUI(g); //creates a frame
 
 
 
