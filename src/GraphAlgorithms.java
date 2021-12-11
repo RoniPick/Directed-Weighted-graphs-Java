98678bb216e8aff01cfee53cf8ff3d740289a39b
Almog-David <almog.david99@gmail.com>
2021-12-11 18:46:21 +0200

    more test + data files

--- src/GraphAlgorithms.java
+++ src/GraphAlgorithms.java
@@ -10,291 +10,280 @@ import java.util.*;
 import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
-import org.junit.jupiter.api.parallel.Resources;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GraphAlgorithms implements DirectedWeightedGraphAlgorithms {
     private Graph graph;
 
     public GraphAlgorithms() {
-            this.graph = new Graph();
+        this.graph = new Graph();
+    }
+
+    public GraphAlgorithms(Graph graph) {
+        this.graph = graph;
+    }
+
+
+    @Override
+    public void init(DirectedWeightedGraph g) {
+        this.graph = (Graph) g;
+    }
+
+    @Override
+    public DirectedWeightedGraph getGraph() {
+        return this.graph;
+    }
+
+    @Override
+    public DirectedWeightedGraph copy() {
+        Graph g = new Graph();
+        g.setItercounter(this.graph.getItercounter());
+        g.setEdges(this.graph.getEdges());
+        g.setNodes(this.graph.getNodes());
+        g.setMC(this.graph.getMC());
+        return g;
+    }
+
+    @Override
+    public boolean isConnected() {
+        Iterator nodesIterator = graph.getNodes().entrySet().iterator();
+
+        while (nodesIterator.hasNext()) {
+            HashMap.Entry element = (HashMap.Entry) nodesIterator.next();
+            int curr = ((int) element.getKey());
+            if (!BFS(curr))
+                return false;
         }
+        return true;
+    }
 
-        public GraphAlgorithms(Graph graph) {
-            this.graph = graph;
-        }
-
-
-        @Override
-        public void init(DirectedWeightedGraph g) {
-            this.graph = (Graph) g;
-        }
-
-        @Override
-        public DirectedWeightedGraph getGraph() {
-            return this.graph;
-        }
-
-        @Override
-        public DirectedWeightedGraph copy() {
-            Graph g = new Graph();
-            g.setItercounter(this.graph.getItercounter());
-            g.setEdges(this.graph.getEdges());
-            g.setNodes(this.graph.getNodes());
-            g.setMC(this.graph.getMC());
-            return g;
-        }
-
-        @Override
-        public boolean isConnected() {
-            Iterator nodesIterator = graph.getNodes().entrySet().iterator();
-
-            while (nodesIterator.hasNext()) {
-                HashMap.Entry element = (HashMap.Entry) nodesIterator.next();
-                int curr = ((int) element.getKey());
-                if (!BFS(curr))
-                    return false;
-            }
-            return true;
-        }
-
-        private Boolean BFS(int n) {
-            LinkedList<Integer> visited = new LinkedList<>(); // we save the nodes id that we visited already
-            LinkedList<Integer> queue = new LinkedList(); // in the queue we will put only the nodes that we can reach to from our node.
-            queue.add(n);
-            while (!queue.isEmpty()) {
-                int temp = queue.poll();
-                Node cur = (Node) graph.getNodes().get(temp);
-                LinkedList<Integer> out = cur.getOutEdge();
-                int index = 0;
-                while (index < out.size()) {
-                    if (!visited.contains(temp)) {
-                        visited.add(temp);
-                        queue.add(out.get(index));
-                    }
-                    index++;
+    private Boolean BFS(int n) {
+        LinkedList<Integer> visited = new LinkedList<>(); // we save the nodes id that we visited already
+        LinkedList<Integer> queue = new LinkedList(); // in the queue we will put only the nodes that we can reach to from our node.
+        queue.add(n);
+        visited.add(n);
+        while (!queue.isEmpty()) {
+            int temp = queue.poll();
+            Node cur = (Node) graph.getNodes().get(temp);
+            LinkedList<Integer> out = cur.getOutEdge();
+            int index = 0;
+            while (index < out.size()) {
+                if (!visited.contains(out.get(index))) {
+                    visited.add(out.get(index));
+                    queue.add(out.get(index));
                 }
-
+                index++;
             }
-            return (visited.size() == graph.nodeSize());
-        }
 
-        @Override
-        public double shortestPathDist(int src, int dest) {
-            if (src == dest || !this.isConnected())
-                return -1;
+        }
+        return (visited.size() == graph.nodeSize());
+    }
+
+    @Override
+    public double shortestPathDist(int src, int dest) {
+        if (this.isConnected() && src != dest) {
             HashMap<Integer, Double> ans = DijkstraLength(src);
             return ans.get(dest);
         }
+        return -1;
+    }
 
-        private HashMap<Integer, Double> DijkstraLength(int src) {
-            Graph g = (Graph) copy();
-            LinkedList<Integer> visited = new LinkedList<>();
-            HashMap<Integer, Double> distance = new HashMap<>();
-            Queue<Node> neighbours = new PriorityQueue<>((v1, v2) -> (int) (v1.getWeight() - v2.getWeight()));
-            Iterator<NodeData> nodes = graph.nodeIter();
-            while (nodes.hasNext()) {
-                Node a = (Node) nodes.next();
-                a.setWeight(Double.MAX_VALUE);
-            }
+    private HashMap<Integer, Double> DijkstraLength(int src) {
+        Graph g = (Graph) copy();
+        LinkedList<Integer> visited = new LinkedList<>();
+        HashMap<Integer, Double> distance = new HashMap<>();
+        Queue<Node> neighbours = new PriorityQueue<>((v1, v2) -> (int) (v1.getWeight() - v2.getWeight()));
+        Iterator<NodeData> nodes = graph.nodeIter();
+        while (nodes.hasNext()) {
+            Node a = (Node) nodes.next();
+            a.setWeight(Double.MAX_VALUE);
+        }
 
-            Node first = (Node) g.getNodes().get(src);
-            first.setWeight(0);
-            neighbours.add(first);
-            while (!neighbours.isEmpty()) {
-                Node curr = neighbours.poll(); // we need to take the node with the minimum weight.
-                LinkedList<Integer> out = curr.getOutEdge();
-                int index = 0;
-                while (index < out.size()) {
-                    if (g.getEdges().get(curr.getKey()).containsKey(out.get(index))) {
-                        if (relax((Edge) g.getEdges().get(curr.getKey()).get(out.get(index)))) {
-                            NodeData temp = g.getNode(out.get(index));
-                            if (!distance.containsKey(temp.getKey()))
-                                distance.put(temp.getKey(), temp.getWeight());
-                            else {
-                                distance.replace(temp.getKey(), temp.getWeight());
-                            }
-                            if (!visited.contains(g.getNode(out.get(index)).getKey())) {
-                                visited.add(g.getNode(out.get(index)).getKey());
-                                neighbours.add((Node) g.getNode(out.get(index)));
-                            }
+        Node first = (Node) g.getNodes().get(src);
+        first.setWeight(0);
+        neighbours.add(first);
+        while (!neighbours.isEmpty()) {
+            Node curr = neighbours.poll(); // we need to take the node with the minimum weight.
+            LinkedList<Integer> out = curr.getOutEdge();
+            int index = 0;
+            while (index < out.size()) {
+                if (g.getEdges().get(curr.getKey()).containsKey(out.get(index))) {
+                    if (relax((Edge) g.getEdges().get(curr.getKey()).get(out.get(index)))) {
+                        NodeData temp = g.getNode(out.get(index));
+                        if (!distance.containsKey(temp.getKey()))
+                            distance.put(temp.getKey(), temp.getWeight());
+                        else {
+                            distance.replace(temp.getKey(), temp.getWeight());
+                        }
+                        if (!visited.contains(g.getNode(out.get(index)).getKey())) {
+                            visited.add(g.getNode(out.get(index)).getKey());
+                            neighbours.add((Node) g.getNode(out.get(index)));
                         }
                     }
-                    index++;
                 }
+                index++;
             }
-            return distance;
+        }
+        return distance;
+    }
+
+    @Override
+    public List<NodeData> shortestPath(int src, int dest) {
+        HashMap<Integer, NodeData> D = DijkstraPath(src);
+        List<NodeData> ans = new LinkedList<>();
+        NodeData location = graph.getNode(dest);
+        while (location!=null) {
+            int curr = location.getKey();
+            ans.add(0,location);
+            location = D.get(curr);
+        }
+        return ans;
+    }
+
+
+    public HashMap<Integer, NodeData> DijkstraPath(int src) {
+        Graph g = (Graph) copy();
+        LinkedList<Integer> visited = new LinkedList<>();
+        HashMap<Integer, NodeData> previous = new HashMap<>();
+        Queue<Node> neighbours = new PriorityQueue<>((v1, v2) -> (int) (v1.getWeight() - v2.getWeight()));
+        Iterator<NodeData> nodes = graph.nodeIter();
+        while (nodes.hasNext()) {
+            Node a = (Node) nodes.next();
+            a.setWeight(Double.MAX_VALUE);
         }
 
-        @Override
-        public List<NodeData> shortestPath(int src, int dest) {
-            HashMap<Integer, NodeData> D = DijkstraPath(src);
-            List<NodeData> ans = new LinkedList<>();
-            NodeData location = graph.getNode(dest);
-            while (location!=null) {
-                int curr = location.getKey();
-                ans.add(0,location);
-                location = D.get(curr);
-            }
-            return ans;
-        }
-
-
-        public HashMap<Integer, NodeData> DijkstraPath(int src) {
-            Graph g = (Graph) copy();
-            LinkedList<Integer> visited = new LinkedList<>();
-            HashMap<Integer, NodeData> previous = new HashMap<>();
-            Queue<Node> neighbours = new PriorityQueue<>((v1, v2) -> (int) (v1.getWeight() - v2.getWeight()));
-            Iterator<NodeData> nodes = graph.nodeIter();
-            while (nodes.hasNext()) {
-                Node a = (Node) nodes.next();
-                a.setWeight(Double.MAX_VALUE);
-            }
-
-            Node first = (Node) g.getNodes().get(src);
-            first.setWeight(0);
-            neighbours.add(first);
-            while (!neighbours.isEmpty()) {
-                Node curr = neighbours.poll(); // we need to take the node with the minimum weight.
-                LinkedList<Integer> out = curr.getOutEdge();
-                int index = 0;
-                while (index < out.size()) {
-                    if (g.getEdges().get(curr.getKey()).get(out.get(index))!=null) {
-                        if (relax((Edge) g.getEdges().get(curr.getKey()).get(out.get(index)))) {
-                            NodeData temp = g.getNode(out.get(index));
-                            if (!previous.containsKey(temp.getKey()))
-                                previous.put(temp.getKey(), curr);
-                            else {
-                                previous.replace(temp.getKey(), curr);
-                            }
-                            if (!visited.contains(g.getNode(out.get(index)).getKey())) {
-                                visited.add(g.getNode(out.get(index)).getKey());
-                                neighbours.add((Node) g.getNode(out.get(index)));
-                            }
+        Node first = (Node) g.getNodes().get(src);
+        first.setWeight(0);
+        neighbours.add(first);
+        while (!neighbours.isEmpty()) {
+            Node curr = neighbours.poll(); // we need to take the node with the minimum weight.
+            LinkedList<Integer> out = curr.getOutEdge();
+            int index = 0;
+            while (index < out.size()) {
+                if (g.getEdges().get(curr.getKey()).get(out.get(index))!=null) {
+                    if (relax((Edge) g.getEdges().get(curr.getKey()).get(out.get(index)))) {
+                        NodeData temp = g.getNode(out.get(index));
+                        if (!previous.containsKey(temp.getKey()))
+                            previous.put(temp.getKey(), curr);
+                        else {
+                            previous.replace(temp.getKey(), curr);
+                        }
+                        if (!visited.contains(g.getNode(out.get(index)).getKey())) {
+                            visited.add(g.getNode(out.get(index)).getKey());
+                            neighbours.add((Node) g.getNode(out.get(index)));
                         }
                     }
-                    index++;
                 }
-
+                index++;
             }
-            return previous;
+
         }
+        return previous;
+    }
 
-        private Boolean relax(Edge e) {
-            Node src = (Node) this.graph.getNode(e.getSrc());
-            Node dest = (Node) this.graph.getNode(e.getDest());
-            if (dest.getWeight() <= src.getWeight() + e.getWeight())
-                return false;
+    private Boolean relax(Edge e) {
+        Node src = (Node) this.graph.getNode(e.getSrc());
+        Node dest = (Node) this.graph.getNode(e.getDest());
+        if (dest.getWeight() <= src.getWeight() + e.getWeight())
+            return false;
 
-            dest.setWeight(src.getWeight() + e.getWeight());
-            return true;
-        }
+        dest.setWeight(src.getWeight() + e.getWeight());
+        return true;
+    }
 
-        @Override
-        public NodeData center() {
-            // we need to go through every node and search for the longest path that he have.
-            // then, after we finish to search for the longest path, we will choose the minimum node from the maximum group.
-            int ans = 0;
-            double min = Double.MAX_VALUE;
-            Iterator<NodeData> nodes = graph.nodeIter();
-            while (nodes.hasNext()) {
-                Node a = (Node) nodes.next();
-                HashMap<Integer,Double> weight = DijkstraLength(a.getKey());
-                Iterator itr = weight.values().iterator();
-                double max = 0;
-                while(itr.hasNext()) {
-                    double temp = (Double) itr.next();
-                    if (temp > max)
+    @Override
+    public NodeData center() {
+        // we need to go through every node and search for the longest path that he have.
+        // then, after we finish to search for the longest path, we will choose the minimum node from the maximum group.
+        double max, temp;
+        HashMap<NodeData, Double> ans = new HashMap<>();
+        for (NodeData first : graph.getNodes().values()) {
+            max = 0;
+            for (NodeData second : graph.getNodes().values()) {
+                if (first.getKey() != second.getKey()) {
+                    temp = shortestPathDist(first.getKey(), second.getKey());
+                    if (max < temp)
                         max = temp;
-                    itr.next();
-                }
-
-                if (max < min) {
-                    min = max;
-                    ans = a.getKey();
                 }
             }
-            return graph.getNode(ans);
+            ans.put(first, max);
         }
-        @Override
-        public List<NodeData> tsp(List<NodeData> cities) {
+        return Collections.min(ans.entrySet(), Map.Entry.comparingByValue()).getKey();
+    }
+
+    @Override
+    public List<NodeData> tsp(List<NodeData> cities) {
         if(cities==null || cities.size()==0){
             return null;
         }
         HashMap<Integer,Integer> route = new HashMap<>();
         int start = cities.get(0).getKey();
         while (cities.size()-1>0){
-            HashMap<Integer,NodeData> m = DijkstraPath(cities.remove(0).getKey());
+            HashMap<Integer,NodeData> map = DijkstraPath(cities.remove(0).getKey());
             if(!route.containsKey(cities.get(0).getKey())) {
                 NodeData location = graph.getNode(cities.get(0).getKey());
                 while (location != null) {
                     int curr = location.getKey();
-                    if(m.get(curr)!=null) {
-                        route.put(m.get(curr).getKey(), curr);
+                    if(map.get(curr)!=null) {
+                        route.put(map.get(curr).getKey(), curr);
                     }
-                    location = m.get(curr);
+                    location = map.get(curr);
                 }
             }
         }
-        List<NodeData> r = new LinkedList<>();
-            while (route.get(start)!=null){
-                int key=route.get(start);
-                NodeData n = graph.getNode(start);
-                r.add(n);
-                start=key;
-            }
-            r.add(graph.getNode(start));
+        List<NodeData> ans = new LinkedList<>();
+        while (route.get(start)!=null){
+            int key=route.get(start);
+            NodeData n = graph.getNode(start);
+            ans.add(n);
+            start=key;
+        }
+        ans.add(graph.getNode(start));
+
+        return  ans;}
 
 
 
-        return  r;}
+    @Override
+    public boolean save(String file) {
+        boolean flag = false;
+        JsonToGraph graph = new JsonToGraph().fromGraph(this.graph);
+        System.out.println(graph);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-
-
-        @Override
-        public boolean save(String file) {
-            boolean flag = false;
-            JsonToGraph graph = new JsonToGraph().fromGraph(this.graph);
-            System.out.println(graph);
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
-            try {
-                FileWriter f = new FileWriter(file);
-                f.write(gson.toJson(graph));
-                f.close();
-                flag = true;
-            } catch (IOException e) {
-                e.printStackTrace();
-                flag = false;
-            }
-
-            return flag;
+        try {
+            FileWriter f = new FileWriter(file);
+            f.write(gson.toJson(graph));
+            f.close();
+            flag = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            flag = false;
         }
 
-
-        @Override
-        public boolean load(String file) { // write to the program
-            boolean flag = false;
-
-            try {
-                Gson gson = new Gson();
-                FileReader f = new FileReader(file);
-                JsonReader bufferedReader = new JsonReader(f);
-                JsonToGraph gtj = new JsonToGraph();
-                gtj = gson.fromJson(bufferedReader, new TypeToken<JsonToGraph>() {
-                }.getType());
-                Graph g = gtj.toGraph();
-                this.init(g);
-                // try
-
-                flag = true;
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-                flag = false;
-            }
-
-
-            return flag;
-        }
+        return flag;
     }
+
+
+    @Override
+    public boolean load(String file) { // write to the program
+        boolean flag = false;
+
+        try {
+            Gson gson = new Gson();
+            FileReader f = new FileReader(file);
+            JsonReader bufferedReader = new JsonReader(f);
+            JsonToGraph gtj = new JsonToGraph();
+            gtj = gson.fromJson(bufferedReader, new TypeToken<JsonToGraph>() {
+            }.getType());
+            Graph g = gtj.toGraph();
+            this.init(g);
+
+            flag = true;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            flag = false;
+        }
+        return flag;
+    }
+}
--- src/test/GraphAlgorithmsTest.java
+++ src/test/GraphAlgorithmsTest.java
@@ -1,4 +1,3 @@
-
 import api.DirectedWeightedGraph;
 import api.NodeData;
 import org.junit.jupiter.api.Test;
@@ -45,10 +44,26 @@ class GraphAlgorithmsTest {
         graph.addNode(d);
         graph.connect(d.getKey(),b.getKey(),4);
         assertFalse(g.isConnected());
+
+//        g.load("C:\\Java Projects\\Ex2_OOP\\src\\data\\1000.json");
+//        assertTrue(g.isConnected());
+//
+//        g.load("C:\\Java Projects\\Ex2_OOP\\src\\data\\10000.json");
+//        assertTrue(g.isConnected());
+
+        g.load("C:\\Java Projects\\Ex2_OOP\\src\\data\\G1.json");
+        assertTrue(g.isConnected());
+
+        g.load("C:\\Java Projects\\Ex2_OOP\\src\\data\\G2.json");
+        assertTrue(g.isConnected());
+
+        g.load("C:\\Java Projects\\Ex2_OOP\\src\\data\\G3.json");
+        assertTrue(g.isConnected());
     }
 
     @Test
     void shortestPathDist() {
+        GraphAlgorithms g = new GraphAlgorithms();
         DirectedWeightedGraph graph = g.getGraph();
         Geo g1 = new Geo(22,34,0);
         Node a = new Node(1,0,0,g1);
@@ -68,6 +83,31 @@ class GraphAlgorithmsTest {
         assertEquals(2,ans);
         double ans2 = g.shortestPathDist(c.getKey(),b.getKey());
         assertNotEquals(1, ans2);
+
+        g.load("C:\\Java Projects\\Ex2_OOP\\src\\data\\G1.json");
+        double answer = g.shortestPathDist(1,0);
+        assertEquals(1.8635670623870366, answer);
+
+        g.load("C:\\Java Projects\\Ex2_OOP\\src\\data\\G1.json");
+        double answer2 = g.shortestPathDist(1,0);
+        assertEquals(1.8635670623870366, answer2);
+
+        g.load("C:\\Java Projects\\Ex2_OOP\\src\\data\\G2.json");
+        double answer3 = g.shortestPathDist(8,26);
+        assertEquals(1.2039873817393092, answer3);
+
+        g.load("C:\\Java Projects\\Ex2_OOP\\src\\data\\G2.json");
+        double answer4 = g.shortestPathDist(9,23);
+        assertEquals(1.0350683385093797, answer4);
+
+        g.load("C:\\Java Projects\\Ex2_OOP\\src\\data\\G3.json");
+        double answer5 = g.shortestPathDist(2,9);
+        assertEquals(0.9484993508901526, answer5);
+
+        g.load("C:\\Java Projects\\Ex2_OOP\\src\\data\\G3.json");
+        double answer6 = g.shortestPathDist(7,10);
+        assertEquals(1.069288137885289, answer6);
+        assertNotEquals(1.069288137885255,answer6);
     }
 
 
@@ -81,16 +121,28 @@ class GraphAlgorithmsTest {
         Node b = new Node(2,0,0,g2);
         Geo g3 = new Geo(17,56,0);
         Node c = new Node(3,0,0,g3);
+        Geo g4 = new Geo(17,56,0);
+        Node d = new Node(4,0,0,g4);
         graph.addNode(a);
         graph.addNode(b);
         graph.addNode(c);
-        graph.connect(a.getKey(),b.getKey(),5);
+        graph.addNode(c);
+        graph.addNode(d);
+        graph.connect(a.getKey(),b.getKey(),2);
         graph.connect(b.getKey(),c.getKey(),7);
+        graph.connect(b.getKey(),d.getKey(),1);
         graph.connect(c.getKey(),a.getKey(),3);
         graph.connect(c.getKey(), b.getKey(),1.5);
+        graph.connect(d.getKey(), a.getKey(),4);
+        graph.connect(d.getKey(), c.getKey(),2);
         List <NodeData> ans = g.shortestPath(a.getKey(),c.getKey());
-        assertEquals(3,ans.size());
-
+        assertEquals(4,ans.size());
+        assertEquals(1,ans.get(0).getKey());
+        assertEquals(2,ans.get(1).getKey());
+        assertEquals(4,ans.get(2).getKey());
+        assertEquals(3,ans.get(3).getKey());
+        double weight = g.shortestPathDist(a.getKey(),c.getKey());
+        assertEquals(5, weight);
     }
 
     @Test
@@ -109,13 +161,11 @@ class GraphAlgorithmsTest {
         graph.connect(a.getKey(),b.getKey(),5);
         graph.connect(a.getKey(),c.getKey(),2);
         graph.connect(b.getKey(),c.getKey(),7);
-        graph.connect(b.getKey(),c.getKey(),9);
+        graph.connect(b.getKey(),a.getKey(),9);
         graph.connect(c.getKey(),a.getKey(),3);
         graph.connect(c.getKey(), b.getKey(),1.5);
         NodeData ans = g.center();
         assertEquals(3,ans.getKey());
-        Geo g4 = new Geo(10,10,0);
-        Node d = new Node(4,0,0,g4);
 
         g.load("C:\\Java Projects\\Ex2_OOP\\src\\data\\G1.json");
         NodeData n = g.center();
@@ -125,13 +175,22 @@ class GraphAlgorithmsTest {
         NodeData n2 = g.center();
         assertEquals(0, n2.getKey());
 
+//        g.load("C://Java Projects//Ex2_OOP//src//data//1000.json");
+//        NodeData n3 = g.center();
+//        assertEquals(362, n3.getKey());
+//
+//        g.load("C:\\Java Projects\\Ex2_OOP\\src\\data\\10000.json");
+//        NodeData n4 = g.center();
+//        assertEquals(3846, n4.getKey());
 
-
+        g.load("C:\\Java Projects\\Ex2_OOP\\src\\data\\G3.json");
+        NodeData n5 = g.center();
+        assertEquals(40, n5.getKey());
     }
 
     @Test
     void tsp() {
-        g.load("C:\\Java Projects\\Ex2_OOP\\src\\data\\G1.json");
+        g.load("C://Java Projects//Ex2_OOP//src//data//G1.json");
         List<NodeData> l = new LinkedList<>();
         l.add(g.getGraph().getNode(6));
         l.add(g.getGraph().getNode(8));
